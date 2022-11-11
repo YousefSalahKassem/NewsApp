540702453f27f584b8d3fec7213666aca67b2aaa
kortobaa <kortobaa>
2022-11-11 16:28:36 +0200

    Initial Commit

---
+++ newsApp Watch App/newsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsAppApp.swift
+//  newsApp Watch App
+//
+//  Created by kortobaa on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct newsApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ newsApp/newsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsAppApp.swift
+//  newsApp
+//
+//  Created by kortobaa on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct newsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
